e814debd6bf47d100f731c19eab1ec587ac56006
Avii144 <avinashpilli992@gmail.com>
2025-06-29 23:12:31 +0530

    testing with routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello from server");
+// app.get("/hello", (req, res) => {
+//   res.send("Hello from get");
+// });
+// app.post("/hello", (req, res) => {
+//   res.send("Hello from post");
+// });
+// app.delete("/hello", (req, res) => {
+//   res.send("Hello deleted ");
+// });
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello from hello");
+// });
+// app.use("/", (req, res) => {
+//   res.send("Hello from server");
+// });
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstname: "avinash", lastname: "pilli" });
 });
+
 app.listen(3000, () => {
   console.log("server is successfully listening to port 3000");
 });
