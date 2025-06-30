5970df2717c6f6ffc45958a032fee193b2eb5360
Avii144 <avinashpilli992@gmail.com>
2025-06-30 23:34:36 +0530

    initialized database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstname: "Avinash",
+    lastname: "Pilli",
+    emailId: "avinash@gmail.com",
+    password: "avinash",
+  });
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(500).send("somethig is wrong message:" + err.message);
+  }
+});
 connectDB()
   .then(() => {
     console.log("database connected");
