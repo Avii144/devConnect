584ee109a603ac830ee0ea3ab5bb42bdf329a950
Avii144 <avinashpilli992@gmail.com>
2025-06-30 10:22:01 +0530

    wild-card error handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,17 @@
 const express = require("express");
 
 const app = express();
-
-// app.get("/hello", (req, res) => {
-//   res.send("Hello from get");
-// });
-// app.post("/hello", (req, res) => {
-//   res.send("Hello from post");
-// });
-// app.delete("/hello", (req, res) => {
-//   res.send("Hello deleted ");
-// });
-
-// app.use("/hello", (req, res) => {
-//   res.send("Hello from hello");
-// });
-// app.use("/", (req, res) => {
-//   res.send("Hello from server");
-// });
-
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "avinash", lastname: "pilli" });
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("error");
+  }
+});
+app.get("/getuserData", (req, res) => {
+  try {
+    throw new Error("afdasd");
+  } catch (err) {
+    res.status(500).send("Somehing is error");
+  }
 });
 
 app.listen(3000, () => {
