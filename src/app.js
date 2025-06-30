4b97057653a5732594a90d36baa65a45b8aac3ef
Avii144 <avinashpilli992@gmail.com>
2025-07-01 00:39:12 +0530

    get API s

--- src/app.js
+++ src/app.js
@@ -2,19 +2,43 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
-
+app.use(express.json());
+//signup api
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstname: "Avinash",
-    lastname: "Pilli",
-    emailId: "avinash@gmail.com",
-    password: "avinash",
-  });
+  const user = new User(req.body);
+
+  ////you can use below method  for hardcoded values for dynamic one you need to use above method
+  // const user = new User({
+  //   firstname: "Avinash",
+  //   lastname: "Pilli",
+  //   emailId: "avinash@gmail.com",
+  //   password: "avinash",
+  // });
   try {
     await user.save();
     res.send("user added successfully");
   } catch (err) {
     res.status(500).send("somethig is wrong message:" + err.message);
+  }
+});
+//get user by email
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("something is wrong :" + err.message);
+  }
+});
+//feed api GET/feed--get all the users from the feed
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Something went wrong");
   }
 });
 connectDB()
