e0c777e23a37b125e44e46332d0b187a4c3b1b7b
Avii144 <avinashpilli992@gmail.com>
2025-07-02 17:17:03 +0530

    created schema for connection req

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
-const connectionRequestSchema = new mongoose.Schema({
-  fromUserId: {
-    type: mongoose.Schema.Types.ObjectId,
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["ignore", "interested", "accepted", "rejected"],
+        message: "{VALUE} is incorrect status type",
+      },
+    },
   },
-  toUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-  },
-  status: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
